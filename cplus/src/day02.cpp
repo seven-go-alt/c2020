b753312de09f9c37b021536452b52ba4dd7e927f
caisiying <2838171527@qq.com>
2026-04-17 20:06:03 +0800

    add 02

---
+++ cplus/src/day02.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a = 10;
+    int b = 20;
+    int sum = a + b;
+    cout << "The sum of " << a << " and " << b << " is: " << sum << '\n';
+    return 0;
+}
